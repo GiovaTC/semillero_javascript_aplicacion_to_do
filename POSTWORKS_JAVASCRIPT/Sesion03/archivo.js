67bea95f00d7fd453f37bd8747a19c8caf934353
GiovaTC <90740161+GiovaTC@users.noreply.github.com>
2023-11-17 12:09:13 -0500

    Add files via upload

---
+++ POSTWORKS_JAVASCRIPT/Sesion03/archivo.js
@@ -0,0 +1,13 @@
+var isCompleted = false;
+
+function markTaskAsCompleted() {
+	isCompleted = true;
+}
+/*
+function martTaskAsCompleted() {
+	isCompleted = !isCompleted;
+}
+*/
+function toggleTaskCompleted() {
+	isCompleted = !isCompleted;
+}
